fc36c5751fafe4e6cde2f734e2d00c993b837692
valdimarp20 <valdimarp20@ru.is>
2022-09-22 15:36:52 +0000

    old main

--- src/solver/main.cpp
+++ src/solver/main.cpp
@@ -9,24 +9,19 @@ int main(int argc, char* argv[]) {
                   << std::endl;
         return 1;
     }
-    const char* ipAddress = argv[1];
-    int port1 = atoi(argv[2]);
-    int port2 = atoi(argv[3]);
-    int port3 = atoi(argv[4]);
-    int port4 = atoi(argv[5]);
 
     PuzzleSolver puzzleSolver;
-    UdpClient client = UdpClient(ipAddress);
+    UdpClient client = UdpClient(argv[1]);
     UdpPortScanner scanner = UdpPortScanner(&client);
 
-    if (port1)
-
-        if (argc == 2) {
-            puzzleSolver = PuzzleSolver(ipAddress, &scanner);
-        } else {
-            puzzleSolver = PuzzleSolver(ipAddress, port1, port2, port3, port4, &scanner);
-        }
+    if (argc == 2) {
+        puzzleSolver = PuzzleSolver(argv[1], &scanner);
+    } else {
+        puzzleSolver = PuzzleSolver(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]),
+                                    atoi(argv[5]), &scanner);
+    }
     puzzleSolver.printPuzzlePorts();
+    // puzzleSolver.solvePuzzles();
 
     return 0;
 }
